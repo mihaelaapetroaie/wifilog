09bd293190ec68eb46028dd759192ec1ef5e96ec
Mihaela Apetroaie-Cristea <apetroaiecristeamihaela@gmail.com>
2015-06-30 08:53:15 +0000

    wifi log with sudo iwlist wlan0 scan

--- wifi.py
+++ wifi.py
@@ -1,41 +1,44 @@
 import subprocess
 import datetime
 
-def process_scan_output(output):
-	output=output.decode()
-	lines=output.split("\n")
+def scan():
+        run_scan=subprocess.Popen(["sudo", "iwlist", "wlan0", "scan"], stdout=subprocess.PIPE)
+        output, err = run_scan.communicate()
+        output=output.decode()
+        index=output.find("\n")
+        output=output[index+1:]
+        return output
 
+	
+
+def is_start(line):
+	return line.strip()[:4]=="Cell"
+	
+
+def nicer(lines):
+	index=0
 	devices=[]
-	i=0
-
- 	# cut the scanning line
-	lines=lines[1:]
-
-	# process remaining lines, each containing device
-	for line in lines:
-		line=line.split("\t")
-		devices.append(line[i+1:])
+	for i,line in enumerate(lines):
+		if is_start(line):
+			if i>=1:
+				devices.append(lines[i-index:i])
+			index=1
+		else:
+			index=index+1
+	devices.append(lines[len(lines)-index:])
 	return devices
 
-
-def scan():
-	run_scan=subprocess.Popen(["sudo", "iwlist", "wlan0", "scan"], stdout=subprocess.PIPE)
-	output, err = run_scan.communicate()
-	output=output.decode()
-	index=output.find("\n")
-	output=output[index+1:]
-	return output
-
-logged_devices = set()
-
+a_log=[]
+																	
 while True:
 	devices=scan()
+	lines=devices.split("\n")
+	devices=nicer(lines)
 	for device in devices:
-		if device[0] not in logged_devices:
-			logged_devices.add(device[0])
-			info=scan()
-			print(datetime.datetime.now())
-			print (info)
+		a=device[0].strip().split()
+		a=a[len(a)-1:]
+		if a not in a_log:
+			a_log.append(a)
+			print ("\n".join(device))
 			print()
-																				
-
+		
